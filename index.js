c63ecdbd8f6a6cc6e8b3c2be7a134befb3e1f32f
Pjotrs Baskakovs <baskakovs@me.com>
2022-06-27 10:33:56 +0300

    commit tree ed11045fb5b244bd4c28c4ffef6348b0ba22bc8f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(array, name){
+    let arrayFilter = array.filter(function(array){
+      if(array.toUpperCase() === name.toUpperCase()){
+        return name}
+    })
+    return arrayFilter}
+
+    function fuzzyMatch(array, nameInput){
+        let arrayFilter = array.filter(function (name){
+            if(name[0]== nameInput[0] && name[1] == nameInput[1]){
+              return(name)
+          }
+        })
+        return arrayFilter
+      }
+
+      function matchName(array, string){
+        let arrayFilter = array.filter(function (item){
+          if (item['name']==string){
+            return( item['name'])
+          }
+        })
+        return arrayFilter
+      }
